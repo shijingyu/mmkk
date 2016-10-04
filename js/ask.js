7f628c6a054083bfda8811ce218ca5fcc12d2408
soFly <1351370237@qq.com>
2016-10-04 11:17:54 +0800

    zwj-mkStone1.0.1

---
+++ js/ask.js
@@ -0,0 +1,48 @@
+$(function(){
+	$('.nav li').click(function(){
+              $('.nav li').removeClass('active');
+              $(this).addClass('active');
+
+            });
+
+});
+ $(function(){
+
+ 	$('#btn').click(function(){
+ 		var textarea=$('textarea').val();
+		$("#comment").append("<p>"+textarea+"</p>");
+		
+	});
+ });  
+ // $(function(){
+
+ // 	$('#Toggle').click(function(){
+
+ // 		$('section').slideToggle(1000);
+
+	// // $('.main-sidebar').animate(width:'0px','slow');
+
+ // 	})
+ // }) 
+   
+
+// jQuery(document).ready(function($) { 
+//     var isLateralNavAnimating = false; 
+//     //o打开或关闭导航菜单 
+//     $('#Toggle').on('click', function(event) { 
+//         event.preventDefault(); 
+//         //若动画正在进行，则停止  
+//         if (!isLateralNavAnimating) { 
+//             if ($(this).parents('.csstransitions').length > 0) 
+//                 isLateralNavAnimating = true; 
+ 
+//             $('aside').toggleClass('none-sidebar'); 
+//             $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() { 
+//                 //动画结束 
+//                 isLateralNavAnimating = false; 
+//             }); 
+//         } 
+//     }); 
+// });
+  
+ 
