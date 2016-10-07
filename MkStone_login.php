f2b9ded21ca39cbf4b80af112123fe442631355c
shijingyu <shijy675497282@163.com>
2016-10-07 14:59:49 +0800

    PHP-1.0.6加入session

--- MkStone_login.php
+++ MkStone_login.php
@@ -4,23 +4,44 @@
       //  exit("错误执行");
    // }//检测是否有submit操作 
 
+session_start();
 include('MkStone_Connect.php');//链接数据库
     $name = $_POST['username'];//post获得用户名表单值
-    $passowrd = $_POST['password'];//post获得用户密码单值
+    $password = $_POST['password'];//post获得用户密码单值
 
-    if ($name && $passowrd){//如果用户名和密码都不为空
-             $sql = "select * from stone where usename = '$name' and password='$passowrd'";//检测数据库是否有对应的username和password的sql
-             $result = mysql_query($sql);//执行sql
-             $rows=mysql_num_rows($result);//返回一个数值
-             if($rows){//0 false 1 true           
-                     echo "
-                    <script>
-                            alert('登录成功');location.href='article.html'  
-                    </script>
+    //if ($name && $password){//如果用户名和密码都不为空
+             $sql = "select * from stone where usename = '$name' and password='$password'";//检测数据库是否有对应的username和password的sql
+             
+$check_query = mysql_query($sql);
+if($result = mysql_fetch_array($check_query)){
+    //登录成功
+    $_SESSION['username'] = $name;
+    $_SESSION['userid'] = $result['id'];
+    //echo $name,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
 
-                ";          //登录成功并跳转到文章页面
-                   
-                   exit;
+    echo "<<script>
+
+    alert('登录成功');location.href='article.html'
+
+
+    </script>";
+
+
+  //            $result = mysql_query($sql);//执行sql
+  //            $rows=mysql_num_rows($result);//返回一个数值
+  //           // $result_check=mysql_fetch_array($reslt);
+  //            if($rows){//0 false 1 true           
+  //               //      echo "
+  //               //     <script>
+  //               //             alert('登录成功');location.href='article.html'  
+  //               //     </script>
+
+  //               // ";          //登录成功并跳转到文章页面
+  //                  $_SESSION['username']=$name;
+  //                  $_SESSION['userid']=$result_check['id'];
+                 
+  // echo $name,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
+                   exit();
              }else{
              
                 echo "
@@ -32,6 +53,6 @@ include('MkStone_Connect.php');//链接数据库
              }
              
 
-    }
+    //}
     mysql_close();//关闭数据库
 ?>
