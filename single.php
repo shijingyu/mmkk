336f9fa5a90d9d2e309138c16472fcc303368c87
shijingyu <945090896@qq.com>
2016-10-09 15:24:50 +0800

    修复下载详情页面1.1.2

--- single.php
+++ single.php
@@ -1,49 +1,47 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=3">
     <title>Download</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/single.css">
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="http://cdn.bootcss.com/respond.js/3.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body>
-   <!-- 头部开始 -->
-  <header>
-  <nav class="navbar navbar-fixed-top navbar-default" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">mkStone</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href="index.html"><img src="images/artical_03.png" alt=""></a>
-    </div>
+</head>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-       <li><a href="index.html">首页</a></li>
-            <li><a href="article.php">文章</a></li>
-            <li><a href="ask.html">问答</a></li>
-            <li class="active"><a href="download.php">资源</a></li>
-            <li><a href="about.html">关于</a></li>
-      </ul>
-      
-
-      <!-- 账户没有登录前显示的界面 -->
-      <!-- <ul class="nav navbar-nav navbar-right">
+<body>
+    <!-- 头部开始 -->
+    <header>
+        <nav class="navbar navbar-fixed-top navbar-default" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">mkStone</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.html"><img src="images/artical_03.png" alt=""></a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">首页</a></li>
+                        <li><a href="article.html">文章</a></li>
+                        <li><a href="ask.html">问答</a></li>
+                        <li class="active"><a href="download.html">资源</a></li>
+                        <li><a href="about.html">关于</a></li>
+                    </ul>
+                    <!-- 账户没有登录前显示的界面 -->
+                    <!-- <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">登录注册<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -53,42 +51,40 @@
           </ul>
         </li>
       </ul> -->
-       <!-- 结束 -->
+                    <!-- 结束 -->
+                    <!-- 账户登录后显示 -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="images/artical_09.png" style="width: 18px;" alt=""><span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="article.html">个人资料</a></li>
+                                <li><a href="article.html">退出</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- 结束 -->
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </header>
+    <!-- 头部结束 -->
+    <!-- 正文开始 -->
+    <div class="content">
+        <!-- 居中容器 -->
+        <div class="container">
+            <!-- 栅格 -->
+            <!-- 路径导航 -->
+            <ol class="breadcrumb">
+                <li><a href="#">主页</a></li>
+                <li><a href="#">资源</a></li>
+                <li class="active">type</li>
+            </ol>
 
 
-       <!-- 账户登录后显示 -->
-       <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="images/artical_09.png" style="width: 18px;" alt="" ><span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="article.html">个人资料</a></li>
-            <li><a href="article.html">退出</a></li>
-          </ul>
-        </li>
-      </ul>
-      <!-- 结束 -->
 
 
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-</header>
-<!-- 头部结束 -->
-
-
-<!-- 正文开始 -->
-  <div class="content">
-    <!-- 居中容器 -->
-    <div class="container">
-    <!-- 栅格 -->
-
-    <!-- 路径导航 -->
-          <ol class="breadcrumb">
-            <li><a href="#">主页</a></li>
-            <li><a href="#">资源</a></li>
-            <li class="active">type</li>
-          </ol>
-    
 <?php  
 
 
@@ -108,103 +104,104 @@ while($row=mysql_fetch_assoc($result))
 {
 
 echo"
-  <div class=‘row’>
-         <!-- single页的左边 -->
-        <div class=’single_l‘   col-md-10>
 
 
-         <h1>".$row['resname']."</h1>
-      
-           <div class='single-ms col-md-8' >
-             <p><span>大小</span>：".$row['space']." <span><br>软件分类</span>：".$row['resfenlei']."<br>
-<span>语言</span>：".$row['language']." <br><span>更新日期</span>：".$row['uptime']."<br>
-<span>支持系统</span>：".$row['support']."</p>
-            <a href='".$row['http']."'><button type='bu.contentton' class='btn btn-primary'>高速下载</button></a>
-           </div>
-
-          </div>
 
 
-           <div class='xsjis'>
-             <p class='ptitle'>软件详情</p>
-            <p>".$row['rcontent']."</p>
-   
-          </div>
-  </div>
-      
+            <div class='row'>
+                <!-- single页的左边 -->
+                <div class='single_l   col-md-10'>
+                    <h1>".$row['resname']."</h1>
+                    <!-- 软件简短描述 -->
+                    <div class='single-ms ''>
+                        <p class='ptitle'>软件属性</p>
+                        <p><span>大小</span>：".$row['space']." <span><br>软件分类</span>：".$row['resfenlei']."
+                            <br>
+                            <span>语言</span>：".$row['language']."
+                            <br><span>更新日期</span>：".$row['uptime']."
+                            <br>
+                            <span>支持系统</span>：".$row['support']."</p>
+                        <a href='".$row['http']."'>
+                            <button type='bu.contentton' class='btn btn-primary'>高速下载</button>
+                        </a>
+                    </div>
+                    <div class='jianjie'>
+                        <p class='ptitle'>软件详情</p>
+                        <p>".$row['rcontent']."
+                        </p>
+                       
+                    </div>
+                    
+                   
+                </div>
+
+                <!-- single页左边-end -->
 
 
-";
- }
-}
+"; }}
 
-?>
-      
-        <div class="single_r col-md-2">
-               <p class="p1">
-                  本站收集了各种实用的PC和移动端应用，团队不定时进行软件的发布及补档，如果您发现资源过期了请及时留言联系我们。<br>
-                  xy960722@gmail.com
-               </p>
-               <p class="p2">              
-                  <p>如果喜欢我们的网站，请支持我们！</p>
-                  <img style="width:180px" src="images/erweima.jpg" alt="">
-               </p>
-           </div>
-           <!-- single右边-end -->
+   ?>             
+                <div class="single_r col-md-2">
+                    <p class="p1">
+                        本站收集了各种实用的PC和移动端应用，团队不定时进行软件的发布及补档，如果您发现资源过期了请及时留言联系我们。
+                        <br> xy960722@gmail.com
+                    </p>
+                    <p class="p2">
+                        <p>如果喜欢我们的网站，请支持我们！</p>
+                        <img style="width:180px" src="images/erweima.jpg" alt="">
+                    </p>
+                </div>
+               
 
-      </div>
-      <!-- 栅格-end -->
 
+
+                <!-- single右边-end -->
+            </div>
+            <!-- 栅格-end -->
+        </div>
+        <!-- 居中容器-end -->
     </div>
-    <!-- 居中容器-end -->
-      
-  </div>
-<!-- 正文-end -->
-  <!-- 底部开始 -->
- <footer>
-   <div class="container">
-     <p>Copyright © 2016 Mkstone All rights reserved. <span><a href="">赞助</a></span></p>
-   </div>
- </footer>
- <!-- 底部结束 -->
-
- <!-- 侧边栏开始 -->
- <ul class="sidebar">
-    <li class="li2">
-      <a href="">收藏</a>
-    </li>
-    <li class="li3">
-      <a href="">热门</a>
-    </li>
-    <li class="li4">
-      <a href="">热词</a>
-    </li>
-    <li class="li5">
-      <a href="">搜索</a>
-    </li>
-    <li class="li6">
-      <a href="">咨询</a>
-    </li>
-    <li class="li7" id="gotop"></li>
-  </ul>  
-<!-- 侧边栏-end -->
-
-
-
-
-
-
+    <!-- 正文-end -->
+    <!-- 底部开始 -->
+    <footer>
+        <div class="container">
+            <p>Copyright © 2016 MkStone All rights reserved. <span><a href="">赞助</a></span></p>
+        </div>
+    </footer>
+    <!-- 底部结束 -->
+    <!-- 侧边栏开始 -->
+    <ul class="sidebar">
+        <li class="li2">
+            <a href="">收藏</a>
+        </li>
+        <li class="li3">
+            <a href="">热门</a>
+        </li>
+        <li class="li4">
+            <a href="">热词</a>
+        </li>
+        <li class="li5">
+            <a href="">搜索</a>
+        </li>
+        <li class="li6">
+            <a href="">咨询</a>
+        </li>
+        <li class="li7" id="gotop"></li>
+    </ul>
+    <!-- 侧边栏-end -->
     <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <!--  侧边栏js -->
+    <script>
+    $(document).ready(function() {
+        $("#gotop").click(function() {
+            $("html,body").animate({
+                scrollTop: 0
+            }, 300);
+        });
+    })
+    </script>
+    <!-- 侧边栏js-end -->
+</body>
 
-<!--  侧边栏js -->
-<script>
-$(document).ready(function(){
-  $("#gotop").click(function(){
-  $("html,body").animate({scrollTop:0},300);
-});
-})
-</script>
-<!-- 侧边栏js-end -->
-  </body>
 </html>
